1856e247e63a5e12322f5202d9da6c2c9e6b7243
Dhanvanth SenthilKumar <dhanvanth1610@gmail.com>
2026-07-09 14:16:56 +0530

    Create 242-ValidAnagram

---
+++ 242-ValidAnagram/242-ValidAnagram.java
@@ -0,0 +1,15 @@
+// Last updated: 7/9/2026, 2:16:56 PM
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        Map<Character,Integer> hm =new HashMap();
+        for(char ch :s.toCharArray()){
+            hm.put(ch,hm.getOrDefault(ch,0)+ 1);
+        }for(char ch :t.toCharArray()){
+            hm.put(ch,hm.getOrDefault(ch,0) -1);
+        }
+        for(int x : hm.values()){
+            if(x!=0) return false;
+        }
+        return true;
+    }
+}
