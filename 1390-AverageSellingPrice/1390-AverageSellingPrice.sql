5719a5cd4803f23f7fae76f508394705265ba652
Dhanvanth SenthilKumar <dhanvanth1610@gmail.com>
2026-07-09 14:15:00 +0530

    Create 1390-AverageSellingPrice

---
+++ 1390-AverageSellingPrice/1390-AverageSellingPrice.sql
@@ -0,0 +1,15 @@
+-- Last updated: 7/9/2026, 2:15:00 PM
+SELECT
+    p.product_id,
+    ROUND(
+        IFNULL(
+            SUM(u.units * p.price) / SUM(u.units),
+            0
+        ),
+        2
+    ) AS average_price
+FROM Prices p
+LEFT JOIN UnitsSold u
+ON p.product_id = u.product_id
+AND u.purchase_date BETWEEN p.start_date AND p.end_date
+GROUP BY p.product_id;
