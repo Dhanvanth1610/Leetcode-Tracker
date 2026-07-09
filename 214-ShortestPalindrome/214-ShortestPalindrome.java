3c772a4598c7b75c2f582e94d45b6ac99f15b5fc
Dhanvanth SenthilKumar <dhanvanth1610@gmail.com>
2026-07-09 14:17:12 +0530

    Create 214-ShortestPalindrome

---
+++ 214-ShortestPalindrome/214-ShortestPalindrome.java
@@ -0,0 +1,25 @@
+// Last updated: 7/9/2026, 2:17:13 PM
+class Solution {
+    public String shortestPalindrome(String s) {
+        String rev = new StringBuilder(s).reverse().toString();
+        String str = s + "#" + rev;
+
+        int[] lps = new int[str.length()];
+
+        for (int i = 1; i < str.length(); i++) {
+            int j = lps[i - 1];
+
+            while (j > 0 && str.charAt(i) != str.charAt(j)) {
+                j = lps[j - 1];
+            }
+
+            if (str.charAt(i) == str.charAt(j)) {
+                j++;
+            }
+
+            lps[i] = j;
+        }
+
+        return rev.substring(0, s.length() - lps[str.length() - 1]) + s;
+    }
+}
