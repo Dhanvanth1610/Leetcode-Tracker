1f9f17cd096460fd901ad6ac1762fc3c2e001d19
Dhanvanth SenthilKumar <dhanvanth1610@gmail.com>
2026-07-12 18:33:32 +0530

    Create 38-CountAndSay

---
+++ 38-CountAndSay/38-CountAndSay.cpp
@@ -0,0 +1,26 @@
+// Last updated: 7/12/2026, 6:33:33 PM
+class Solution {
+public:
+    string countAndSay(int n) {
+        string result = "1";
+
+        for (int i = 2; i <= n; i++) {
+            string temp = "";
+            int count = 1;
+
+            for (int j = 1; j <= result.size(); j++) {
+                if (j < result.size() && result[j] == result[j - 1]) {
+                    count++;
+                } else {
+                    temp += to_string(count);
+                    temp += result[j - 1];
+                    count = 1;
+                }
+            }
+
+            result = temp;
+        }
+
+        return result;
+    }
+};
