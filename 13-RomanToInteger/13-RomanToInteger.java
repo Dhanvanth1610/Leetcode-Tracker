d248690486a748619395118c1a08257162c0903b
Dhanvanth SenthilKumar <dhanvanth1610@gmail.com>
2026-07-12 18:34:15 +0530

    Create 13-RomanToInteger

---
+++ 13-RomanToInteger/13-RomanToInteger.java
@@ -0,0 +1,31 @@
+// Last updated: 7/12/2026, 6:34:15 PM
+class Solution {
+    public int romanToInt(String s) {
+        int sum = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            int value = getValue(s.charAt(i));
+
+            if (i + 1 < s.length() && value < getValue(s.charAt(i + 1))) {
+                sum -= value;
+            } else {
+                sum += value;
+            }
+        }
+
+        return sum;
+    }
+
+    private int getValue(char c) {
+        switch (c) {
+            case 'I': return 1;
+            case 'V': return 5;
+            case 'X': return 10;
+            case 'L': return 50;
+            case 'C': return 100;
+            case 'D': return 500;
+            case 'M': return 1000;
+        }
+        return 0;
+    }
+}
