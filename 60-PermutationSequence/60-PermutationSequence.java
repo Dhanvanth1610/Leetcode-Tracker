0b7117efc53f0873ebcf056f3fc09edc3776c066
Dhanvanth SenthilKumar <dhanvanth1610@gmail.com>
2026-07-12 18:32:52 +0530

    Create 60-PermutationSequence

---
+++ 60-PermutationSequence/60-PermutationSequence.java
@@ -0,0 +1,26 @@
+// Last updated: 7/12/2026, 6:32:52 PM
+class Solution {
+    public String getPermutation(int n, int k) {
+        List<Integer> numbers = new ArrayList<>();
+        int[] fact = new int[n + 1];
+        fact[0] = 1;
+
+        for (int i = 1; i <= n; i++) {
+            fact[i] = fact[i - 1] * i;
+            numbers.add(i);
+        }
+
+        k--;
+
+        StringBuilder ans = new StringBuilder();
+
+        for (int i = n; i >= 1; i--) {
+            int index = k / fact[i - 1];
+            ans.append(numbers.get(index));
+            numbers.remove(index);
+            k %= fact[i - 1];
+        }
+
+        return ans.toString();
+    }
+}
